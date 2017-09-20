33365efca62231de9f733c1343cdf8e8dfd6f030
kevin ponek <kponek@berkeley.edu>
2017-09-20 16:41:37 -0700

    hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,16 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    count = 0
+    arr2 = []
+    a.each do |i|
+    	x = Integer(i) + 2
+    	if (x % 2 == 0) &&  !(arr2.include? x) && (x < 10)
+    		arr2.push(x)
+    		count += x
+    	end
+    end
+    return count
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,19 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  return (a+b)*(a+b)
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
 
+  #selection sort
+  return a.sort.map {|num| num + 1} 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  str = first_name + " "
+  return str + last_name
 end
 
 def blockin_time(a)
